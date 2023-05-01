292b0620624d2dbb051bb4380be8a3ef893f01d7
junkyo <124774799+junkyo974@users.noreply.github.com>
2023-05-01 13:35:04 +0900

    swagger 적용

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output");
+
 const postsRouter = require('./routes/posts');
 const usersRouter = require("./routes/users.js");
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get('/', (req, res) => {
     res.send('/signup 회원가입, /login 로그인');
